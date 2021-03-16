e681ea3bc12797628c1d556ce5dd5f9500f634e6
Kamil_IT <kamillo.gronek@gmail.com>
2021-03-16 11:50:56 +0100

    Updates

---
+++ precache-manifest.c3a7f15a309ea2ab7d928f0ff6d74d01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a14a9005d699e53b4b9",
+    "url": "/React-Library/static/css/main.e0c5aa05.chunk.css"
+  },
+  {
+    "revision": "1a14a9005d699e53b4b9",
+    "url": "/React-Library/static/js/main.a8408c86.chunk.js"
+  },
+  {
+    "revision": "86143a30fd167b474e95",
+    "url": "/React-Library/static/js/runtime~main.fcca0e6e.js"
+  },
+  {
+    "revision": "b0b59bce31dc7943dc0c",
+    "url": "/React-Library/static/js/2.37a19cae.chunk.js"
+  },
+  {
+    "revision": "15de37ee07882eaf285d48d1fa4f543d",
+    "url": "/React-Library/index.html"
+  }
+];
